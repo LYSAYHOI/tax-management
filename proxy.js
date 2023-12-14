df3c5caec6fbbac43c427a7eb8f50c8ac4c9088d
Ly Say Hoi <l.sayhoi.001@gmail.com>
2023-12-15 01:27:09 +0700

    update code

--- proxy.js
+++ proxy.js
@@ -6,7 +6,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 app.use('/query/**', createProxyMiddleware({
     target: 'https://hoadondientu.gdt.gov.vn:30000',
     changeOrigin: true,
-    secure: false,
+    // secure: false,
     onProxyRes: function (proxyRes, req, res) {
         proxyRes.headers['Access-Control-Allow-Origin'] = '*';
     }
