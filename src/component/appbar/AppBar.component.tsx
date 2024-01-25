ac4377e7f0f3a2fa39228fc876cd7d2460bc6b31
Ly Say Hoi <l.sayhoi.001@gmail.com>
2024-01-26 01:26:23 +0700

    add appbar

---
+++ src/component/appbar/AppBar.component.tsx
@@ -0,0 +1,70 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import AdbIcon from "@mui/icons-material/Adb";
+
+export default function AppBarComponent() {
+  const pages = ["Hóa Đơn Điện Tử", "Gộp File Excel Danh Sách Hóa Đơn"];
+  return (
+    <AppBar position="static" color="primary">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            TAX
+          </Typography>
+
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                variant="contained"
+                color="success"
+                key={page}
+                onClick={() => {}}
+                sx={{ mx: 1, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
