b90daf5456bf2eff2d9eb624420966c15b5990b9
Ly Say Hoi <l.sayhoi.001@gmail.com>
2024-01-26 01:30:14 +0700

    fix http request

--- src/util/HttpRequest.ts
+++ src/util/HttpRequest.ts
@@ -1,5 +1,4 @@
-import axios from "axios";
-import { RETRY_URL } from "../util/Constant";
+import axios, { AxiosError } from "axios";
 
 const instance = axios.create({
   // baseURL: `https://tax-management-proxy-service-sxxvb4l3sq-as.a.run.app/query/invoices`,
@@ -8,42 +7,28 @@ const instance = axios.create({
   timeout: 1000 * 5
 });
 
-instance.interceptors.request.use(
-  (config) => {
-    document.getElementById("loader-span")?.classList.add("loader");
-    config.headers.Authorization = `Bearer ${localStorage.getItem("at")}`;
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+const getAuthorizationHeader = () => ({
+  Authorization: `Bearer ${localStorage.getItem("at")}`
+})
 
-instance.interceptors.response.use(
-  (res) => {
-    document.getElementById("loader-span")?.classList.remove("loader");
-    return res;
-  },
-  (err) => {
-    document.getElementById("loader-span")?.classList.remove("loader");
-    if (err.response?.status === 403 && RETRY_URL.includes(err.config.url)) {
-      return instance.request(err?.config);
-    } else if (err.response?.status === 401) {
-      window.location.href = "/login";
-      localStorage.clear();
-      return;
-    } else {
-      return Promise.reject(err);
-    }
+const handleUnauthorizationError = (err: AxiosError) => {
+  if (err.response?.status === 401) {
+    localStorage.clear();
+    window.location.href = "/login";
   }
-);
+  return Promise.reject(err);
+}
 
-const get = (url: string, params: any) => {
-  return instance.get(url, { params });
+const Get = (url: string, params: any) => {
+  return instance.get(url, { params, headers: { ...getAuthorizationHeader() } }).catch((err: AxiosError) =>
+    handleUnauthorizationError(err)
+  );
 };
 
-const getFile = (url: string, params: any) => {
-  return instance.get(url, { params, responseType: "arraybuffer" });
+const GetFile = (url: string, params: any) => {
+  return instance.get(url, { params, responseType: "arraybuffer", headers: { ...getAuthorizationHeader() } }).catch((err: AxiosError) =>
+    handleUnauthorizationError(err)
+  );
 };
 
-export { get, getFile };
+export { Get, GetFile };
