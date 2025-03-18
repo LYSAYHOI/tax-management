fd5ec734f354c0704e839af2105734eeff03daf1
Ly Say Hoi <l.sayhoi.001@gmail.com>
2025-03-19 00:03:00 +0700

    add nginx config for docker

--- dockerfile
+++ dockerfile
@@ -11,6 +11,5 @@ COPY ./nginx/nginx.conf /etc/nginx/conf.d/configfile.template
 COPY --from=build /react-app/build /usr/share/nginx/html
 
 ENV PORT 8080
-ENV HOST 0.0.0.0
 
 CMD sh -c "envsubst '\$PORT' < /etc/nginx/conf.d/configfile.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'"
--- src/util/HttpRequest.ts
+++ src/util/HttpRequest.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
   // baseURL: `https://tax-management-proxy-service-sxxvb4l3sq-as.a.run.app/query/invoices`,
   // baseURL: `https://hoadondientu.gdt.gov.vn:30000/query/invoices`,
   // baseURL: "https://tax-management-server-sxxvb4l3sq-as.a.run.app",
-  baseURL: "http://localhost:8080",
+  baseURL: "http://localhost:8080/api",
   timeout: 1000 * 15
 });
 
