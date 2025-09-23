82f98afa1a5bacac86b5b0e310482aee3e06859d
Ly Say Hoi <l.sayhoi.001@gmail.com>
2025-09-24 01:41:41 +0700

    add proixy

--- proxy.js
+++ proxy.js
@@ -1,12 +1,20 @@
 const express = require("express");
-var cors = require('cors')
+const { HttpsProxyAgent } = require("https-proxy-agent");
+const cors = require('cors')
+
 const app = express();
 app.use(cors());
+
+// Proxy config
+const proxyUrl = "http://qxgxhp:vsbdvuwa@160.191.50.127:3488";
+const agent = new HttpsProxyAgent(proxyUrl);
+
 const { createProxyMiddleware } = require('http-proxy-middleware');
 app.use('/query/**', createProxyMiddleware({
     target: 'https://hoadondientu.gdt.gov.vn:30000',
     changeOrigin: true,
     // secure: false,
+    agent: agent, // Use proxy server
     onProxyRes: function (proxyRes, req, res) {
         proxyRes.headers['Access-Control-Allow-Origin'] = '*';
     }
